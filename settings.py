e28c868201b49cb3c8e713f554b752e79228a164
skywurz <wpddjsky@sbcglobal.net>
2017-01-19 10:29:28 -0800

    Create settings.py
    this is the new settings file

---
+++ settings.py
@@ -0,0 +1,25 @@
+# settings.py
+
+####################Globals##########################
+
+####Folder Paths###
+usbcardpath = "usbstore"
+localstore = "localstore"
+scriptpath = "scripts"
+
+####Enable Components####
+ftp = True
+gpsenable = True
+recordenable = True
+testing = False
+
+####SSIDs####
+homessid = 'HomeWifi'
+mobilessid = 'MobilWiFi'
+#mobilessid = 'test'
+workssid = 'WorkWiFi'
+
+####FTP Credentials####
+ftpserveradder = 'ftp.yourwebsite.com'
+ftpuser = 'user@yourwebsite.com'
+ftppass = 'pass'
