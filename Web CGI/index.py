6808be0280f93d65f02dbef04cd907f55c3fea59
skywurz <skywurz@github.com>
2017-01-23 16:49:43 -0800

    fixed bad css directory

--- Web CGI/index.py
+++ Web CGI/index.py
@@ -15,10 +15,10 @@ print('''
 		<title>Rasp-Cam</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<!--[if lte IE 8]><script src="./assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="./assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="./assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="./assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
 	</head>
 	<body>
 
@@ -176,11 +176,11 @@ print('''<div class="content">
 			</div>
 
 		<!-- Scripts -->
-			<script src="./assets/js/jquery.min.js"></script>
-			<script src="./assets/js/skel.min.js"></script>
-			<script src="./assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="./assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="./assets/js/main.js"></script>
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="../assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
 
 	</body>
 </html>''')
