e89282947bf4d54b57f3f55130ae62a7bbec6463
skywurz <skywurz@github.com>
2017-01-24 14:55:31 -0800

    fixed reboot script

--- Web CGI/reboot.py
+++ Web CGI/reboot.py
@@ -14,16 +14,13 @@ print('''
 		<!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
 	</head>
-	<body>''')
-    print('<div class="featuresg">')
-print('<article>')
-<i class="icon fa-refresh fa-spin fa-3x fa-fw"></i>
+	<body>
+''')
+print(''' 
+<i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
 <span class="sr-only">Loading...</span>
-print(''' <div class="content">
-<h3>GPS</h3>
-<!-- <p>GPS Connectivity and </p> -->
-</div>
-</div>
+<div class="content">
+<h3>Rebooting</h3>
 </article>''')
-os.system("reboot now")
+os.system("sudo reboot now")
 
