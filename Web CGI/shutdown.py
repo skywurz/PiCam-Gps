dd1275a75d3cb6cc08dd6f1aec917bd1245abaaf
skywurz <wpddjsky@sbcglobal.net>
2017-01-25 11:14:58 -0800

    Create shutdown.py

---
+++ Web CGI/shutdown.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3 -u
+import cgi
+import subprocess
+import os
+import time
+
+print("Content-type: text/html \n\n", flush=True)
+print('''
+<html>
+	<head>
+		<title>Rasp-Cam</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
+		<meta http-equiv="refresh" content="3; url=/cgi-bin/index.py" />
+	</head>
+	<body>
+  <div style=margin:0 auto;">
+<h3>Shutting Down</h3>
+<i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+<span class="sr-only">Loading...</span>
+<div class="content">
+</article>
+</div>
+''', flush=True)
+time.sleep(5)
+os.system('sudo shutdown now')
