9e0e9c99aef779a72deb318419c62b5f01607fb1
skywurz <wpddjsky@sbcglobal.net>
2017-01-28 10:32:40 -0800

    Update settings.py

--- scripts/settings.py
+++ scripts/settings.py
@@ -8,7 +8,7 @@ localstore = "localstore"
 scriptpath = "scripts"
 
 ####Enable Components####
-ftp = True
+ftpenabled = True
 gpsenable = True
 recordenable = True
 testing = False
@@ -23,3 +23,9 @@ workssid = 'WorkWiFi'
 ftpserveradder = 'ftp.yourwebsite.com'
 ftpuser = 'user@yourwebsite.com'
 ftppass = 'pass'
+
+####GPS Settings####
+gpslooptime = 140 #How often the gps updates location (and photo)subtract 10 sec
+gpsmappoints = 47 #Number of points to keep before overwriting them
+gpsnonfixreset = 15 #Number of times the gps will break out of timeout before it resets the whole system. Hint: gpsnonfixreset*gpstimeouttime = how long befor systemreset in Min
+gpstimeouttime = 1 #The ammount of time IN MINUNETS the system waits for gps fix before resetting the gps USB port and killing gps thread
